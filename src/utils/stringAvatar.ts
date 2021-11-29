8c119df1c96fd7f22ea891fb0c79f694485d75da
zero179 <imon.fenko@gmail.com>
2021-11-29 12:29:36 +0100

    fixed user registration

--- src/utils/stringAvatar.ts
+++ src/utils/stringAvatar.ts
@@ -1,12 +1,22 @@
 import stringToColor from "./stringToColor";
 
+function createAbr(name: string | null | undefined){
+    if (typeof name === "undefined" || name === null){
+        return "UN"
+    }
+    if (name.includes(' ')) {
+        return `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`
+    }
+    return `${name.split(' ')[0][0]}${name.split(' ')[0][1]}`
+};
+
 const stringAvatar = (name: string | null | undefined) => ({
     sx: {
         fontSize: 16,
         fontWeight: 800,
         bgcolor: stringToColor(name),
     },
-    children: name ? `${name.split(' ')[0][0]}${name.split(' ')[1][0]}` : '',
+    children: createAbr(name),
 });
 
 export default stringAvatar
